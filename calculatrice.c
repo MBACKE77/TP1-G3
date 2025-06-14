78776568d6ba80304f889b7aa61dd6152fbcfea9
MBACKE77 <sn871510@gmail.com>
2025-06-14 12:56:20 +0100

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -1,11 +1,39 @@
 //directive de compilation par saliou
+#define PI 3.14
+#ifdef DEBUG
+    printf("Mode debug activé\n");
+#endif
 
 //foction de soustraction par Diakhoumpa
+int soustraction(int a, int b) {
+    return a - b;
+}
+
 
 //fonction de Multiplication pat Rayan
+int multiplication(int a, int b) {
+    return a * b;
+}
 
 // fonction de division par Saliou
+float division(int a, int b) {
+    if (b == 0) {
+        printf("Erreur : division par zéro\n");
+        return 0;
+    }
+    return (float)a / b;
+}
 
 // fonction d'addition par Diakhoumpa
+int addition(int a, int b) {
+    return a + b;
+}
 
 // fonction de puissance par Rayan
+int puissance(int base, int exposant) {
+    int resultat = 1;
+    for(int i = 0; i < exposant; i++) {
+        resultat *= base;
+    }
+    return resultat;
+}
