361abe103384fce7675176da14656648cd48dc89
MBACKE77 <sn871510@gmail.com>
2025-06-14 12:16:30 +0100

    Create calculatrice.c

---
+++ calculatrice.c
@@ -0,0 +1,11 @@
+//directive de compilation par saliou
+
+//foction de soustraction par Diakhoumpa
+
+//fonction de Multiplication pat Rayan
+
+// fonction de division par Saliou
+
+// fonction d'addition par Diakhoumpa
+
+// fonction de puissance par Rayan
